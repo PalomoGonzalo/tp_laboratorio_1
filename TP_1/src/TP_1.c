dd429bf0fef6ddebf25d48faa1262d8ac6f1ef64
PalomoGonzalo <89616271+PalomoGonzalo@users.noreply.github.com>
2021-09-09 15:52:24 -0300

    Subido desde la web

---
+++ TP_1/src/TP_1.c
@@ -0,0 +1,138 @@
+/*
+ ============================================================================
+ Name        : TP_1.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "funciones.h"
+
+int main(void) {
+
+	setbuf(stdout, NULL);
+	float numeroUno=0;
+	float numeroDos=0;
+	char seguir = 's';
+	int opcion = 0;
+	int aux;
+	int flagNumUno;
+	int flagNumDos;
+	int auxDatos;
+	float resultadoDivision;
+	while (seguir=='s')
+	{
+		auxDatos=verificarDatos(flagNumUno, flagNumDos);
+		printf("1- Ingresar 1er operando (A=x)\n");
+		printf("2- Ingresar 2do operando (B=y)\n");
+		printf("3- Calcular la suma (A+B)\n");
+		printf("4- Calcular la resta (A-B)\n");
+		printf("5- Calcular la division (A/B)\n");
+		printf("6- Calcular la multiplicacion (A*B)\n");
+		printf("7- Calcular el factorial (A!)\n");
+		printf("8- Calcular todas las operacione\n");
+		printf("9- Salir\n");
+		scanf("%d",&opcion);
+
+		switch (opcion)
+		{
+			case 1:
+				flagNumUno=pedirNumeros(&numeroUno, "ingrese el primer operando\n");
+				break;
+			case 2:
+				flagNumDos=pedirNumeros(&numeroDos,"ingrese el segundo operando\n");
+				break;
+
+			case 3:
+
+				if(auxDatos==1)
+				{
+					sumarNumeros(numeroUno,numeroDos);
+				}
+				else
+				{
+					mensajeErrorDatos(auxDatos);
+				}
+
+				break;
+
+			case 4:
+
+				if(auxDatos==1)
+				{
+					restarNumeros(numeroUno,numeroDos);
+				}
+				else
+				{
+					mensajeErrorDatos(auxDatos);
+				}
+				break;
+
+			case 5:
+
+				if(auxDatos==1)
+				{
+					aux=dividirNumeros(numeroUno, numeroDos, &resultadoDivision);
+					mensajeDivison(aux,resultadoDivision,numeroUno,numeroDos);
+				}
+				else
+				{
+					mensajeErrorDatos(auxDatos);
+				}
+				break;
+
+			case 6:
+
+				if(auxDatos==1)
+				{
+					multiplicarNumeros(numeroUno, numeroDos);
+				}
+				else
+				{
+					mensajeErrorDatos(auxDatos);
+				}
+				break;
+
+			case 7:
+
+				if(auxDatos==1)
+				{
+					factorialNumeros(numeroUno);
+					factorialNumeros(numeroDos);
+				}
+				else
+				{
+					mensajeErrorDatos(auxDatos);
+				}
+
+				break;
+
+			case 8:
+
+				if(auxDatos==1)
+				{
+					sumarNumeros(numeroUno,numeroDos);
+					restarNumeros(numeroUno,numeroDos);
+					aux=dividirNumeros(numeroUno, numeroDos, &resultadoDivision);
+					mensajeDivison(aux,resultadoDivision,numeroUno,numeroDos);
+					multiplicarNumeros(numeroUno, numeroDos);
+					factorialNumeros(numeroUno);
+					factorialNumeros(numeroDos);
+				}
+				else
+				{
+					mensajeErrorDatos(auxDatos);
+				}
+				break;
+			case 9:
+				seguir='n';
+				break;
+		}
+	}
+
+	return EXIT_SUCCESS;
+}
---
+++ TP_1/src/funciones.c
@@ -0,0 +1,125 @@
+/*
+ * funciones.c
+ *
+ *  Created on: 9 sep. 2021
+ *      Author: palom
+ */
+
+#include "funciones.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int pedirNumeros(float* numeros,char* mensaje)
+{
+	int flag=1;
+	printf(mensaje);
+	fflush(stdin);
+	scanf("%f",numeros);
+	return flag;
+}
+int verificarDatos(int flagUno, int flagDos)
+{
+	int retorno=0;
+	if(flagUno!=1&&flagDos!=1)
+	{
+		retorno=0;
+	}
+	if(flagUno==1&&flagDos!=1)
+	{
+		retorno=-1;
+	}
+	 if(flagUno!=1&&flagDos==1)
+	{
+		retorno=-2;
+	}
+	if(flagUno==1&&flagDos==1)
+	{
+		retorno=1;
+	}
+	return retorno;
+}
+void mensajeErrorDatos(int retorno)
+{
+	if(retorno==0)
+	{
+		printf("no hay datos para los operandos\n\n");
+	}
+	if(retorno==-1)
+	{
+		printf("no hay dato para el segundo operando\n\n");
+	}
+	if(retorno==-2)
+	{
+		printf("no hay dato para el primer operando\n\n");
+	}
+
+
+}
+float sumarNumeros(float a,float b)
+{
+	float resultado;
+	resultado=a+b;
+	printf("la suma de %0.2f+%0.2f es = %0.2f \n",a,b,resultado);
+	return resultado;
+}
+float restarNumeros(float a,float b)
+{
+	float resultado;
+	resultado=a-b;
+	printf("la resta de %0.2f-%0.2f es = %0.2f \n",a,b,resultado);
+	return resultado;
+}
+int dividirNumeros (float a,float b,float* resultado)
+{
+	int retorno;
+
+	if(b!=0)
+	{
+		*resultado=a /b;
+
+	}
+	else
+	{
+		retorno=1;
+	}
+	return retorno;
+
+}
+void mensajeDivison(int retorno,float resultado,float a,float b)
+{
+	if(retorno==1)
+	{
+		printf("no se puede divivir por cero\n");
+	}
+	else
+	{
+		printf("la division de %0.2f/%0.2f es = %0.2f \n",a,b,resultado);
+	}
+
+}
+
+float multiplicarNumeros(float a,float b)
+{
+	float resultado;
+	resultado=a*b;
+	printf("la multiplicacion de %0.2f*%0.2f es = %0.2f \n",a,b,resultado);
+	return resultado;
+}
+int factorialNumeros(int numero)
+{
+	int factorial = 1;
+	while (numero < 0)
+	{
+		printf("no se puede sacar factor numero negativo, reingrese el numero\n");
+		scanf("%d", &numero);
+	}
+
+	for (int i = 1; i <= numero; i++)
+	{
+		factorial *= i;
+	}
+
+	printf("el factorial de %d es = %d\n",numero,factorial);
+	return factorial;
+
+}
